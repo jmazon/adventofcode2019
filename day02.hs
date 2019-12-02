182259400a9ad322eff3823a58e0fa35a78d108b
Jean-Baptiste Mazon <jb@yavin>
2019-12-02 10:52:43 +0100

    Day 2 simplifications

--- day02.hs
+++ day02.hs
@@ -1,22 +1,26 @@
-{-# LANGUAGE TypeApplications #-}
-
 import qualified Data.Vector as V
 import Data.Vector ((!),(//))
 import Data.List.Split (linesBy)
 import Data.List (find)
 
-step v p = case v!p of
-  1 -> (Just (p+4), v // [(v!(p+3),v!(v!(p+1)) + v!(v!(p+2)))])
-  2 -> (Just (p+4), v // [(v!(p+3),v!(v!(p+1)) * v!(v!(p+2)))])
-  99 -> (Nothing,v)
-
-evaluate v arg1 arg2 = go (Just 0) (v // [(1,arg1),(2,arg2)]) where
-  go Nothing v = v!0
-  go (Just p) v = uncurry go (step v p)
+evaluate v arg1 arg2 = go (v // [(1,arg1),(2,arg2)]) 0 where
+  go v p = case v!p of
+    1 -> go (v // [(v!(p+3),v!(v!(p+1)) + v!(v!(p+2)))]) (p+4)
+    2 -> go (v // [(v!(p+3),v!(v!(p+1)) * v!(v!(p+2)))]) (p+4)
+    99 -> v!0
 
 main = do
-  prg <- V.fromList . map (read @Int) . linesBy (== ',') <$> getContents
+  prg <- V.fromList . map read . linesBy (== ',') <$> getContents
   print $ evaluate prg 12 2
+
+  -- I initially searched (and found and starred part 2) with a bash
+  -- oneliner, arguments between 0 and 100 inclusive.  Then I cleaned
+  -- up the code and used a quarter-plane enumeration that worked for
+  -- infinite inputs.  Then /u/sophiebits showed me (any many others)
+  -- that the input was bounded, so I'm simplifying to that even
+  -- though the 2D enumeration was kind of elegant.  (and actually
+  -- shorter, lol)
+  -- https://old.reddit.com/r/adventofcode/comments/e4u0rw/2019_day_2_solutions/f9f9uow/
   let Just (noun,verb) = find ((== 19690720) . uncurry (evaluate prg))
-                         [ (i-j,j) | i <- [0..], j <- [0..i] ]
+                         [ (i,j) | i <- [0..99], j <- [0..99] ]
   print $ 100 * noun + verb
