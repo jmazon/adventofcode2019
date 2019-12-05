a14d4bd8e7c346b4283936a6a1d267e5b60b2ce5
Jean-Baptiste Mazon <jb@yavin>
2019-12-05 11:17:01 +0100

    Day 5 cleanup, round one

--- day05.hs
+++ day05.hs
@@ -5,29 +5,24 @@ import Data.List.Split (linesBy)
 evaluate :: Vector Int -> [Int] -> [Int]
 evaluate v input = go v input 0 where
   go v i p = case opcode of
-      1 -> go (write v (v!(p+3)) (param mode1 (v!(p+1)) + param mode2 (v!(p+2))))
-              i (p+4)
-      2 -> go (write v (v!(p+3)) (param mode1 (v!(p+1)) * param mode2 (v!(p+2))))
-              i (p+4)
-      3 -> let (h:t) = i in go (v // [(v!(p+1),h)]) t (p+2)
-      4 -> param mode1 (v!(p+1)) : go v i (p+2)
-      5 -> go v i (if param mode1 (v!(p+1)) /= 0 then param mode2 (v!(p+2))
-                   else p+3)
-      6 -> go v i (if param mode1 (v!(p+1)) == 0 then param mode2 (v!(p+2))
-                   else p+3)
-      7 -> go (write v (v!(p+3)) $ fromEnum $
-               param mode1 (v!(p+1)) < param mode2 (v!(p+2))) i (p+4)
-      8 -> go (write v (v!(p+3)) $ fromEnum $
-               param mode1 (v!(p+1)) == param mode2 (v!(p+2))) i (p+4)
+      1 -> go (out 3 (inParam 1 + inParam 2)) i (p+4)
+      2 -> go (out 3 (inParam 1 * inParam 2)) i (p+4)
+      3 -> let (h:t) = i in go (out 1 h) t (p+2)
+      4 -> inParam 1 : go v i (p+2)
+      5 -> go v i (if inParam 1 /= 0 then inParam 2 else p+3)
+      6 -> go v i (if inParam 1 == 0 then inParam 2 else p+3)
+      7 -> go (out 3 $ fromEnum $ inParam 1  < inParam 2) i (p+4)
+      8 -> go (out 3 $ fromEnum $ inParam 1 == inParam 2) i (p+4)
       99 -> []
     where modInst = v!p
-          mode1 = modInst `div` 100 `mod` 10
-          mode2 = modInst `div` 1000 `mod` 10
-          mode3 = modInst `div` 10000
+          mode 1 = modInst `div` 100 `mod` 10
+          mode 2 = modInst `div` 1000 `mod` 10
+          mode 3 = modInst `div` 10000
           opcode = modInst `mod` 100
           param 0 p = v!p
           param 1 p = p
-  write v p n = v // [(p,n)]
+          inParam i = param (mode i) (v!(p+i))
+          out i n | mode i == 0 = v // [(v!(p+i),n)]
 
 main = do
   prg <- V.fromList . map read . linesBy (== ',') <$> getContents
