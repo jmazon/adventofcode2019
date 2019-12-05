672814b5bfe7870a36046f96423130f0d6c74354
Jean-Baptiste Mazon <jb@yavin>
2019-12-05 11:44:22 +0100

    Day 5 cleanup, round three

--- day05.hs
+++ day05.hs
@@ -4,27 +4,24 @@ import Data.List.Split (linesBy)
 
 evaluate :: Vector Int -> [Int] -> [Int]
 evaluate v input = go v input 0 where
-  go v i p = case opcode of
+  go v i p = case modInst `mod` 100 of
       1 -> binOp (+)
       2 -> binOp (*)
-      3 -> let (h:t) = i in go (out 1 h) t (p+2)
+      3 -> let h:t = i in go (out 1 h) t (p+2)
       4 -> inParam 1 : go v i (p+2)
-      5 -> testOp (/= 0)
-      6 -> testOp (== 0)
+      5 -> condBranchOp (/= 0)
+      6 -> condBranchOp (== 0)
       7 -> binOp ((fromEnum .) .  (<))
       8 -> binOp ((fromEnum .) . (==))
       99 -> []
     where modInst = v!p
-          mode 1 = modInst `div` 100 `mod` 10
-          mode 2 = modInst `div` 1000 `mod` 10
-          mode 3 = modInst `div` 10000
-          opcode = modInst `mod` 100
-          param 0 p = v!p
-          param 1 p = p
-          inParam i = param (mode i) (v!(p+i))
+          mode i = modInst `div` 10^(i+1) `mod` 10
+          decodeParam 0 p = v!p
+          decodeParam 1 p = p
+          inParam i = decodeParam (mode i) (v!(p+i))
           out i n | mode i == 0 = v // [(v!(p+i),n)]
-          binOp o = go (out 3 (inParam 1 `o` inParam 2)) i (p+4)
-          testOp pr = go v i (if pr (inParam 1) then inParam 2 else p+3)
+          binOp op = go (out 3 (inParam 1 `op` inParam 2)) i (p+4)
+          condBranchOp pr = go v i (if pr (inParam 1) then inParam 2 else p+3)
 
 main = do
   prg <- V.fromList . map read . linesBy (== ',') <$> getContents
