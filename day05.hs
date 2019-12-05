176cce7402e4c0312cfdc8829262d9244633d905
Jean-Baptiste Mazon <jb@yavin>
2019-12-05 11:29:19 +0100

    Day 5 cleanup, round two

--- day05.hs
+++ day05.hs
@@ -5,14 +5,14 @@ import Data.List.Split (linesBy)
 evaluate :: Vector Int -> [Int] -> [Int]
 evaluate v input = go v input 0 where
   go v i p = case opcode of
-      1 -> go (out 3 (inParam 1 + inParam 2)) i (p+4)
-      2 -> go (out 3 (inParam 1 * inParam 2)) i (p+4)
+      1 -> binOp (+)
+      2 -> binOp (*)
       3 -> let (h:t) = i in go (out 1 h) t (p+2)
       4 -> inParam 1 : go v i (p+2)
-      5 -> go v i (if inParam 1 /= 0 then inParam 2 else p+3)
-      6 -> go v i (if inParam 1 == 0 then inParam 2 else p+3)
-      7 -> go (out 3 $ fromEnum $ inParam 1  < inParam 2) i (p+4)
-      8 -> go (out 3 $ fromEnum $ inParam 1 == inParam 2) i (p+4)
+      5 -> testOp (/= 0)
+      6 -> testOp (== 0)
+      7 -> binOp ((fromEnum .) .  (<))
+      8 -> binOp ((fromEnum .) . (==))
       99 -> []
     where modInst = v!p
           mode 1 = modInst `div` 100 `mod` 10
@@ -23,6 +23,8 @@ evaluate v input = go v input 0 where
           param 1 p = p
           inParam i = param (mode i) (v!(p+i))
           out i n | mode i == 0 = v // [(v!(p+i),n)]
+          binOp o = go (out 3 (inParam 1 `o` inParam 2)) i (p+4)
+          testOp pr = go v i (if pr (inParam 1) then inParam 2 else p+3)
 
 main = do
   prg <- V.fromList . map read . linesBy (== ',') <$> getContents
