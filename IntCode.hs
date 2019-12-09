d53623cffd0e585c0ef90372b511c54afb4852f9
Jean-Baptiste Mazon <jb@yavin>
2019-12-09 20:01:07 +0100

    Migrate IntCode to RWS (R=(Pos,Rel), S=RAM

--- IntCode.hs
+++ IntCode.hs
@@ -1,13 +1,14 @@
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE FlexibleContexts #-}
 module IntCode (getIntCode,evaluateOld,evaluate) where
 
 import Data.Maybe (fromMaybe)
 import qualified Data.Vector as V
-import Data.Vector (Vector,(!?),(//))
+import Data.Vector (Vector,(!),(!?),(//))
 import Data.List.Split (linesBy)
 import Data.List (intercalate)
 import Control.Arrow (second)
-import Control.Monad.Writer.Lazy
+import Control.Monad.RWS.Lazy
 import Debug.Trace
 
 traceState = False
@@ -20,16 +21,56 @@ type RAM = Vector Int
 
 getIntCode :: IO RAM
 getIntCode = V.fromList . map read . linesBy (== ',') <$> getContents
-  
-evaluateOld prg i j = fst (_evaluate (prg // [(1,i),(2,j)])
-                                     (error "No input to evaluateOld"))
 
-evaluate = (snd .) . _evaluate
+evaluateOld :: RAM -> Int -> Int -> Int
+evaluateOld prg i j = (! 0) $ fst $
+                      execRWS (_evaluate (error "No input to evaluateOld"))
+                              (R 0 0)
+                              (prg // [(1,i),(2,j)])
 
-_evaluate :: RAM -> [Int] -> (Int,[Int])
-_evaluate v0 input = runWriter (go v0 input 0 0) where
-  go v _ r p | traceState && traceShow (v,r,p) False = undefined
-  go v i r p = case modInst `mod` 100 of
+evaluate prg i = snd $ evalRWS (_evaluate i) (R 0 0) prg
+
+_evaluate :: [Int] -> RWS R [Int] RAM ()
+_evaluate input = go input where
+  -- go _ r p | traceState && traceShow (v,r,p) False = undefined
+  go i = do
+    p <- asks position
+    op <- decodeOp <$> readAddr p
+
+    let binOp f name = do
+          (op1,dbgOp1) <- inParam op 1
+          (op2,dbgOp2) <- inParam op 2
+          (out,dbgOp3) <- outParam op 3
+          traceOp [name,dbgOp1,dbgOp2,dbgOp3] $
+            local (advancePos 4) $
+            out (op1 `f` op2) *> go i
+
+        haltOp name = traceOp [name] $ pure ()
+
+        inOp name = do
+          (out,dbgOut) <- outParam op 1
+          traceOp [name,dbgOut] $
+            let h:t = i in out h *> local (advancePos 2) (go t)
+
+        outOp name = do
+          (op,dbgOp) <- inParam op 1
+          traceOp [name,dbgOp] $
+            tell [op] *> local (advancePos 2) (go i)
+
+        condBranchOp pr name = do
+          (op1,dbgOp1) <- inParam op 1
+          (op2,dbgOp2) <- inParam op 2
+          traceOp [name,dbgOp1,dbgOp2] $
+            local (if pr op1 then setPos op2 else advancePos 3) (go i)
+
+        relOp name = do
+          (delta,dbgDelta) <- inParam op 1
+          traceOp [name,dbgDelta] $
+            local (advanceRel delta) $
+            local (advancePos 2) $
+            go i
+
+    case modInst op of
       1  -> binOp (+) "ADD"
       2  -> binOp (*) "MUL"
       3  -> inOp "IN"
@@ -42,56 +83,51 @@ _evaluate v0 input = runWriter (go v0 input 0 0) where
       99 -> haltOp "HCF"
       x  -> error $ "Unknown opcode " ++ show x
 
-    where modInst = fromMaybe (error $ "IP at " ++ show p) (v !? p)
-          mode operand = modInst `div` 10^(operand+1) `mod` 10
+data Op = Op { modInst :: !Int, opInst :: !Int }
 
-          inParam :: Int -> (Int,String)
-          inParam o = case mode o of
-              0 -> (readAddr s,"[" ++ show s ++ "]")
-              1 -> (s,show s)
-              2 -> (readAddr (r+s),"R[" ++ show s ++ "]")
-              x -> error $ "Unknown input addressing mode " ++ show x
-            where s = readAddr (p+o)
+decodeOp :: Int -> Op
+decodeOp n = Op (n `mod` 100) (n `div` 100)
 
-          outParam :: Int -> (Int -> RAM,String)
-          outParam o = case mode o of
-              0 -> (writeAddr      t ,"[" ++ show t ++ "]")
-              2 -> (writeAddr (r + t),"R[" ++ show p ++ "]")
-              x -> error $ "Unknown output addressing mode " ++ show x
-            where t = readAddr (p+o)
+readAddr :: MonadState RAM m => Int -> m Int
+readAddr a | a < 0 = error $ "Read from negative address " ++ show a
+readAddr addr = fromMaybe 0 . (!? addr) <$> get
 
-          binOp op name = traceOp [name,dbgOp1,dbgOp2,dbgOp3] $
-                          go (out (op1 `op` op2)) i r (p+4)
-            where (op1,dbgOp1) = inParam 1
-                  (op2,dbgOp2) = inParam 2
-                  (out,dbgOp3) = outParam 3
+writeAddr :: MonadState RAM m => Int -> Int -> m ()
+writeAddr a n | n < 0 =
+  error $ "Write at negative address " ++ show (a,n)
+writeAddr t n = do
+  v <- get
+  let v' | t < V.length v = v
+         | otherwise = v <> V.replicate (t - V.length v + 1) 0
+      v'' = v' // [(t,n)]
+  n `seq` v'' `seq` put v''
 
-          haltOp name = traceOp [name] $ pure (readAddr 0)
+data Mode = Position | Immediate | Relative deriving (Show,Enum)
 
-          inOp name = traceOp [name,dbgOut] $
-                      let h:t = i in go (out h) t r (p+2)
-            where (out,dbgOut) = outParam 1
+mode :: Op -> Int -> Mode
+mode op operand = toEnum $ opInst op `div` 10^(operand-1) `mod` 10
 
-          outOp name = traceOp [name,dbgOp] $
-                       tell [op] *> go v i r (p+2)
-            where (op,dbgOp) = inParam 1
+data R = R { position :: !Int, relative :: !Int }
 
-          condBranchOp pr name = traceOp [name,dbgOp1,dbgOp2] $
-                                 go v i r (if pr op1 then op2 else p+3)
-            where (op1,dbgOp1) = inParam 1
-                  (op2,dbgOp2) = inParam 2
+setPos p r = r { position = p }
+advancePos delta r@R{position} = r { position = position + delta }
+advanceRel delta r@R{relative} = r { relative = relative + delta }
 
-          relOp name = traceOp [name,dbgDelta] $
-                       go v i (r + delta) (p+2)
-            where (delta,dbgDelta) = inParam 1
+inParam :: (MonadReader R m,MonadState RAM m) => Op -> Int -> m (Int,String)
+inParam op o = do
+  p <- asks position
+  readAddr (p+o) >>= \s -> case mode op o of
+    Position  -> readAddr    s  >>= \v -> pure (v,"[" ++ show s ++ "]")
+    Immediate ->                          pure (s,show s)
+    Relative  -> asks relative >>= \r ->
+                 readAddr (r+s) >>= \v -> pure (v,"R[" ++ show s ++ "]")
 
-          readAddr :: Int -> Int
-          readAddr a | a < 0 = error $ "Read from negative address " ++ show a
-          readAddr addr = fromMaybe 0 (v !? addr)
-
-          writeAddr :: Int -> Int -> RAM
-          writeAddr a n | n < 0 =
-            error $ "Write at negative address " ++ show (a,n)
-          writeAddr t n = v' // [(t,n)] where
-            v' | t < V.length v = v
-               | otherwise = v <> V.replicate (t - V.length v + 1) 0
+outParam :: (MonadReader R m,MonadState RAM m)
+         => Op -> Int -> m (Int -> m (),String)
+outParam op o = do
+  p <- asks position
+  readAddr (p+o) >>= \t -> case mode op o of
+    Position -> pure (writeAddr      t,"[" ++ show t ++ "]")
+    Relative -> asks relative >>= \r ->
+                pure (writeAddr (r + t),"R[" ++ show p ++ "]")
+    x -> error $ "Invalid output addressing mode " ++ show x
