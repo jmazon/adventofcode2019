38a566139575dca8aa433b1099f7842597359599
Jean-Baptiste Mazon <jb@yavin>
2019-12-09 16:29:16 +0100

    Factor IntCode

--- day02.hs
+++ day02.hs
@@ -1,17 +1,9 @@
-import qualified Data.Vector as V
-import Data.Vector ((!),(//))
-import Data.List.Split (linesBy)
+import IntCode
 import Data.List (find)
 
-evaluate v arg1 arg2 = go (v // [(1,arg1),(2,arg2)]) 0 where
-  go v p = case v!p of
-    1 -> go (v // [(v!(p+3),v!(v!(p+1)) + v!(v!(p+2)))]) (p+4)
-    2 -> go (v // [(v!(p+3),v!(v!(p+1)) * v!(v!(p+2)))]) (p+4)
-    99 -> v!0
-
 main = do
-  prg <- V.fromList . map read . linesBy (== ',') <$> getContents
-  print $ evaluate prg 12 2
+  prg <- getIntCode
+  print $ evaluateOld prg 12 2
 
   -- I initially searched (and found and starred part 2) with a bash
   -- oneliner, arguments between 0 and 100 inclusive.  Then I cleaned
@@ -21,6 +13,6 @@ main = do
   -- though the 2D enumeration was kind of elegant.  (and actually
   -- shorter, lol)
   -- https://old.reddit.com/r/adventofcode/comments/e4u0rw/2019_day_2_solutions/f9f9uow/
-  let Just (noun,verb) = find ((== 19690720) . uncurry (evaluate prg))
+  let Just (noun,verb) = find ((== 19690720) . uncurry (evaluateOld prg))
                          [ (i,j) | i <- [0..99], j <- [0..99] ]
   print $ 100 * noun + verb
--- day05.hs
+++ day05.hs
@@ -1,29 +1,6 @@
-import qualified Data.Vector as V
-import Data.Vector (Vector,(!),(//))
-import Data.List.Split (linesBy)
-
-evaluate :: Vector Int -> [Int] -> [Int]
-evaluate v input = go v input 0 where
-  go v i p = case modInst `mod` 100 of
-      1 -> binOp (+)
-      2 -> binOp (*)
-      3 -> let h:t = i in go (out 1 h) t (p+2)
-      4 -> inParam 1 : go v i (p+2)
-      5 -> condBranchOp (/= 0)
-      6 -> condBranchOp (== 0)
-      7 -> binOp ((fromEnum .) .  (<))
-      8 -> binOp ((fromEnum .) . (==))
-      99 -> []
-    where modInst = v!p
-          mode i = modInst `div` 10^(i+1) `mod` 10
-          decodeParam 0 p = v!p
-          decodeParam 1 p = p
-          inParam i = decodeParam (mode i) (v!(p+i))
-          out i n | mode i == 0 = v // [(v!(p+i),n)]
-          binOp op = go (out 3 (inParam 1 `op` inParam 2)) i (p+4)
-          condBranchOp pr = go v i (if pr (inParam 1) then inParam 2 else p+3)
+import IntCode
 
 main = do
-  prg <- V.fromList . map read . linesBy (== ',') <$> getContents
+  prg <- getIntCode
   print $ evaluate prg [1]
   print $ evaluate prg [5]
