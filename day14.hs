e48bef87dc953a6ce52d2c3f7db9dbdbd57a1347
Jean-Baptiste Mazon <jb@darkstar>
2019-12-14 14:26:38 +0100

    Day 14

---
+++ day14.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE FlexibleContexts #-}
+import qualified Data.Map.Strict as M
+import           Data.Map.Strict (Map,(!))
+import           Control.Monad.RWS.Strict
+import           Text.Regex.PCRE
+
+readRecipe = M.fromList . map readReaction . lines where
+  readReaction l = (product,(stoichiometry,ingredients)) where
+    ((product,stoichiometry):ingredients) = parseIngredients $
+                                            reverse shoppingList
+    parseIngredients ((i,n):is) = (i,read n) : parseIngredients is
+    parseIngredients [] = []
+    shoppingList = zip (getAllTextMatches $ l =~ "[A-Z]+")
+                       (getAllTextMatches $ l =~ "[0-9]+")
+
+consume quantity "ORE" = tell (Sum quantity)
+consume quantity product = do
+  create <- (quantity -) <$> gets (M.findWithDefault 0 product)
+  when (create > 0) $ do
+    (stoichiometry,ingredients) <- asks (! product)
+    let q = (create + stoichiometry - 1) `div` stoichiometry
+    forM_ ingredients $ \(i,n) -> consume (q*n) i
+    modify' (M.insertWith (+) product (q*stoichiometry))
+  modify' (M.insertWith (+) product (-quantity))
+
+bsearch f goal = go where
+  go ok excess | excess <= ok+1 = ok
+               | f mid <= goal  = go mid excess
+               | otherwise      = go ok  mid
+    where mid = (ok + excess) `div` 2
+
+main = do
+  recipe <- readRecipe <$> getContents
+  let oreCost fuel = getSum $ snd $ evalRWS (consume fuel "FUEL") recipe M.empty
+      unitOreCost = oreCost 1
+      oreCapacity = 1000000000000
+      minFuel = last $ takeWhile ((< oreCapacity) . oreCost) $
+                iterate (*2) (oreCapacity `div` unitOreCost)
+  print unitOreCost
+  print $ bsearch oreCost oreCapacity minFuel (2*minFuel)
