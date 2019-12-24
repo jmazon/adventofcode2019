cf63bc9e4fc8fc6247beccb2c688f23be77fc4d0
Jean-Baptiste Mazon <jb@yavin>
2019-12-24 19:31:45 +0100

    Day 24: center grids on (0,0)

--- day24.hs
+++ day24.hs
@@ -8,13 +8,13 @@ main = do
   raw <- lines <$> getContents
   guard (length raw == 5)
   guard (all ((== 5) . length) raw)
-  let grid = listArray ((1,1),(5,5)) $ map (== '#') $ concat raw
+  let grid = listArray ((-2,-2),(2,2)) $ map (== '#') $ concat raw
   print $ biodiversity $ firstRepeat step grid
 
-  let gridd = accumArray (flip const) False ((-200,(1,1)),(200,(5,5))) $
+  let gridd = accumArray (flip const) False ((-200,(-2,-2)),(200,(2,2))) $
               map (first ((,) 0)) (assocs grid)
   let gridd' = iterate stepp gridd !! 200
-  print $ length $ filter (gridd'!) $ filter ((/= (3,3)) . snd) $ indices gridd'
+  print $ length $ filter (gridd'!) $ filter ((/= (0,0)) . snd) $ indices gridd'
 
 biodiversity :: Array (Int,Int) Bool -> Int
 biodiversity = sum . zipWith (*) (iterate (*2) 1) . map fromEnum . elems
@@ -32,20 +32,20 @@ step grid = listArray (bounds grid) $ map cell (indices grid) where
 
 stepp :: Array (Int,(Int,Int)) Bool -> Array (Int,(Int,Int)) Bool
 stepp gridd = listArray (bounds gridd) $ map cell (indices gridd) where
-  cell p | (d,(3,3)) <- p = error ("Evaled at level " ++ show d)
+  cell p | (d,(0,0)) <- p = error ("Evaled at level " ++ show d)
          | gridd!p = neighborCount == 1
          | otherwise = neighborCount == 1 || neighborCount == 2
     where neighborCount | abs (fst p) == 200 = 0
                         | otherwise = length $ filter (gridd!) $ concat [leftNeighbor p,upNeighbor p,rightNeighbor p,downNeighbor p]
-          leftNeighbor (d,(_,1)) = [(d-1,(3,2))]
-          leftNeighbor (d,(3,4)) = [(d+1,(i,5)) | i <- [1..5]]
+          leftNeighbor (d,(_,-2)) = [(d-1,(0,-1))]
+          leftNeighbor (d,(0,1)) = [(d+1,(i,2)) | i <- [-2..2]]
           leftNeighbor (d,(i,j)) = [(d,(i,j-1))]
-          upNeighbor (d,(1,_)) = [(d-1,(2,3))]
-          upNeighbor (d,(4,3)) = [(d+1,(5,j)) | j <- [1..5]]
+          upNeighbor (d,(-2,_)) = [(d-1,(-1,0))]
+          upNeighbor (d,(1,0)) = [(d+1,(2,j)) | j <- [-2..2]]
           upNeighbor (d,(i,j)) = [(d,(i-1,j))]
-          rightNeighbor (d,(_,5)) = [(d-1,(3,4))]
-          rightNeighbor (d,(3,2)) = [(d+1,(i,1)) | i <- [1..5]]
+          rightNeighbor (d,(_,2)) = [(d-1,(0,1))]
+          rightNeighbor (d,(0,-1)) = [(d+1,(i,-2)) | i <- [-2..2]]
           rightNeighbor (d,(i,j)) = [(d,(i,j+1))]
-          downNeighbor (d,(5,_)) = [(d-1,(4,3))]
-          downNeighbor (d,(2,3)) = [(d+1,(1,j)) | j <- [1..5]]
+          downNeighbor (d,(2,_)) = [(d-1,(1,0))]
+          downNeighbor (d,(-1,0)) = [(d+1,(-2,j)) | j <- [-2..2]]
           downNeighbor (d,(i,j)) = [(d,(i+1,j))]
