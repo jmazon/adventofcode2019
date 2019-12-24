15b50de5c81742ac024cd6dae0ad2a7918824422
Jean-Baptiste Mazon <jb@yavin>
2019-12-24 01:55:07 +0100

    Cleaner day 23

--- day23.hs
+++ day23.hs
@@ -1,81 +1,84 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# OPTIONS_GHC -Wall #-}
-import IntCode
+{-# LANGUAGE FlexibleContexts, RecursiveDo, LambdaCase #-}
+import           IntCode
 import qualified Data.IntMap.Strict as M
-import Data.Bits
-import Data.List.Split (chunksOf)
-import Control.Concurrent
-import Control.Concurrent.STM
-import Control.Monad.Trans.Free
-import Control.Monad.State.Strict
-import Control.Monad.Extra
+import           Data.Bits (bit,setBit,clearBit)
+import           Data.IORef
+import           Data.List.Split (chunksOf)
+import           Control.Concurrent
+import           Control.Concurrent.STM
+import           Control.Monad.Trans.Free
+import           Control.Monad.State.Strict
+import           Control.Monad.Reader
 
-import Debug.Trace
-
-runNIC :: M.IntMap (TQueue Int) -> TVar Int
-       -> Free IntCodeF ()
-       -> (Int,TQueue Int) -> IO ()
-runNIC chans sem prg (self,chan) = evalStateT (iterM go prg) Nothing where
-  go (Input f) = do
-    v <- liftIO $ atomically $ do
-      mv <- tryReadTQueue chan
-      case mv of
-        Nothing -> modifyTVar' sem (`clearBit` self) *> pure (-1)
-        Just v -> modifyTVar' sem (`setBit` self) *> pure (Val v)
-    f v
-  go (Output v f) = do
+runNIC :: (Int -> (Int,Int) -> IO ()) -> TVar Int -> TQueue Int
+       -> Free IntCodeF () -> Int -> IO ()
+runNIC sendTo sem chan f self = runReaderT (evalStateT (iterM go f) []) False where
+  go (Input cont) = do
+    idle <- ask
+    (idle',v) <- liftIO $ atomically (tryReadTQueue chan) >>=
+      \case Nothing -> do
+              when idle $ atomically $ modifyTVar' sem (`clearBit` self)
+              yield
+              pure (True,-1)
+            Just v -> do
+              atomically $ modifyTVar' sem (`setBit` self)
+              pure (False,Val v)
+    local (const idle') (cont v)
+  go (Output v cont) = do
     liftIO $ atomically $ modifyTVar' sem (`setBit` self)
-    s <- get
-    case s of
-      Nothing -> put (Just (unVal v,Nothing))
-      Just (target,Nothing) -> put (Just (target,Just (unVal v)))
-      Just (target,Just x) -> do
-        let y = unVal v
-        let q = chans M.! target
-        liftIO $ atomically $ do
-          writeTQueue q x
-          writeTQueue q y
-        put Nothing
-    f
+    get >>= \case [x,target] -> do
+                    let y = unVal v
+                    liftIO $ sendTo target (x,y)
+                    put []
+                  vs -> put (unVal v:vs)
+    local (const False) cont
+
+runNAT :: (Int -> (Int,Int) -> IO ()) -> TVar Int -> TQueue Int
+       -> ((Int,Int) -> IO ()) -> (Int -> IO ()) -> IO ()
+runNAT sendTo sem q onUpdate onResume = do
+  buf <- newIORef (error "NAT before idle")
+  forever $ do
+    atomically $ do
+      n <- readTVar sem
+      check (n == 0)
+    flush <- atomically (flushTQueue q)
+    forM_ (chunksOf 2 flush) $ \[x,y] -> writeIORef buf (x,y) *> onUpdate (x,y)
+    (x,y) <- readIORef buf
+    sendTo 0 (x,y)
+    onResume y
+    yield
 
 main :: IO ()
 main = do
   prg <- getIntCode
-  chans <- fmap M.fromList $ forM (255 : [0..49]) $ \i -> do
-    c <- newTQueueIO
-    atomically (writeTQueue c i)
-    return (i,c)
-  
-  sem <- newTVarIO (2^(50 :: Int) - 1)
-  
-  forM_ [0..49] $ \i -> do
-    forkIO $ runNIC chans sem (evaluateF prg) (i,chans M.! i)
+  sem <- newTVarIO (bit 50 - 1)
+  natPackets <- newEmptyMVar
+  natStream <- newChan
+  rec chans <- fmap M.fromList $ do
 
-  nat <- newChan
-  void $ forkIO $ do
-    let q = chans M.! 255
-    255 <- atomically (readTQueue q)
+        let sendTo nic (x,y) = atomically $ do
+              let q = chans M.! nic
+              writeTQueue q x
+              writeTQueue q y
 
-    buf <- newTVarIO Nothing
+        nics <- forM [0..49] $ \i -> do
+          c <- newTQueueIO
+          atomically (writeTQueue c i)
+          void $ forkIO $ runNIC sendTo sem c (evaluateF prg) i
+          return (i,c)
 
-    forever $ do
-      y <- atomically $ do
-        n <- readTVar sem
-        check (n == 0)
-        flush <- flushTQueue q
-        forM_ (chunksOf 2 flush) $ \[x,y] -> writeTVar buf (Just (x,y))
-        b <- readTVar buf
-        (x,y) <- case b of Nothing -> traceM "idle before NAT" *> retry
-                           Just x -> pure x
-        writeTQueue (chans M.! 0) x
-        writeTQueue (chans M.! 0) y
-        return y
-      writeChan nat y
-      whileM ((== 0) <$> readTVarIO sem)
+        nat <- do
+          c <- newTQueueIO
+          void $ forkIO $ runNAT sendTo sem c (void . tryPutMVar natPackets)
+                                              (writeChan natStream)
+          return (255,c)
 
-  print . extractDupe =<< getChanContents nat
+        pure (nat : nics)
+
+  print . snd =<< takeMVar natPackets
+  print . extractDupe =<< getChanContents natStream
 
 extractDupe :: (Show a,Eq a) => [a] -> a
-extractDupe (a:xs@(b:_)) | traceShowId a == b = a
+extractDupe (a:xs@(b:_)) | a == b = a
                          | otherwise = extractDupe xs
 extractDupe _ = error "extractDupe: exhausted input"
