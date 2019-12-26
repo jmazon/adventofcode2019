9373b6c7f91335ec644d7ff554d0a67df248aeb2
Jean-Baptiste Mazon <jb@yavin>
2019-12-26 18:35:43 +0100

    Day 7 cleanup

--- day07.hs
+++ day07.hs
@@ -1,19 +1,24 @@
+-- Day 7: Amplification Circuit
+
 import IntCode
 
 import Data.List (permutations)
 import Data.Function (fix)
 
+-- An amplifier is a transducer, i.e. a stream to stream function.
 amplifier :: RAM -> Int -> Transducer
-amplifier prg p is = evaluate prg (p : is)
+amplifier prg phase is = evaluate prg (phase : is)
 
-chain :: RAM -> [Int] -> Transducer
-chain prg = foldr1 (.) . map (amplifier prg)
+-- A series of amplifiers is simply their functional composition.  Yay FP!
+series :: RAM -> [Int] -> Transducer
+series prg = foldr1 (.) . map (amplifier prg)
 
+-- A feedback loop is built as a series' least fixed point.  Yay lazyness!
 loop :: RAM -> [Int] -> Transducer
-loop prg is ps = fix $ chain prg ps . (is ++)
+loop prg phases = fix . (series prg phases .) . (++)
 
 main :: IO ()
 main = do
   prg <- getIntCode
-  print $ maximum $ map (flip (chain prg) [0]) $ permutations [0..4]
-  print $ maximum $ map (last . loop prg [0]) $ permutations [5..9]
+  print $ maximum $ map (last . ($ [0]) . series prg) $ permutations [0..4]
+  print $ maximum $ map (last . ($ [0]) . loop   prg) $ permutations [5..9]
