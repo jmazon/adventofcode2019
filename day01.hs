0120203b9a4af1953d38f33aca587ffde3f4a8b4
Jean-Baptiste Mazon <jb@yavin>
2019-12-26 16:10:12 +0100

    Clean up day 1

--- day01.hs
+++ day01.hs
@@ -1,5 +1,12 @@
-fuel n = n `div` 3 - 2
+-- Day 1: The Tyranny of the Rocket Equation
+
+fuelRequirements :: Int -> Int
+fuelRequirements mass = mass `div` 3 - 2
+
+main :: IO ()
 main = do
-  ms <- map read . lines <$> getContents
-  print $ sum (map fuel ms)
-  print $ sum (concatMap (tail . takeWhile (> 0) . iterate fuel) ms)
+  moduleMasses <- map read . lines <$> getContents
+  print $ sum $ map fuelRequirements moduleMasses
+  print $ sum
+        $ concatMap (takeWhile (> 0) . tail . iterate fuelRequirements)
+          moduleMasses
