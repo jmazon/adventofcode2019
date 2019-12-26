61732a6ac7aac56b87cfd971f57c08e174c18102
Jean-Baptiste Mazon <jb@yavin>
2019-12-26 18:43:49 +0100

    Day 8 cleanup

--- day08.hs
+++ day08.hs
@@ -1,15 +1,23 @@
-import Data.Function   (on)
+-- Day 8: Space Image Format
+
+import Data.Ord        (comparing)
 import Data.List       (minimumBy,transpose)
 import Data.List.Split (chunksOf)
 
-numOf = (length .) .  filter . (==)
+numOf :: Eq a => a -> [a] -> Int
+numOf = (length .) . filter . (==)
 
+combine :: Char -> Char -> Char
 combine '0' _ = ' '
 combine '1' _ = '*'
 combine  _  b =  b
 
+bg :: Char
+bg = error "Leftover transparent pixels"
+
+main :: IO ()
 main = do
   layers <- chunksOf (6 * 25) <$> getLine
   print $ (*) <$> numOf '1' <*> numOf '2'
-        $ minimumBy (compare `on` numOf '0') layers
-  mapM_ putStrLn $ chunksOf 25 $ map (foldr combine '\a') (transpose layers)
+        $ minimumBy (comparing $ numOf '0') layers
+  mapM_ putStrLn $ chunksOf 25 $ map (foldr combine bg) $ transpose layers
