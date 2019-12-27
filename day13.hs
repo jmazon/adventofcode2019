8a3d19f0359f343ff1e4bac702b50ebe491755f5
Jean-Baptiste Mazon <jb@yavin>
2019-12-27 14:25:42 +0100

    Day 13 cleanup
    Let's be honest, that one was rather clean already.
    
    My biggest frustration here is the use of threading to transfer joystick
    events to the input stream.  Possibly a byproduct of using streams at all.
    
    In the end, this one should end up in the free monad IntCode interface,
    but in the meantime I'm yearning for a select-like function that could
    observe whether a new item has been produced in a lazy list.
    
    Maybe I should go and ask StackOverflow someday.

--- day13.hs
+++ day13.hs
@@ -1,31 +1,42 @@
+-- Day 13: Care Package
+{-# LANGUAGE LambdaCase,DeriveAnyClass #-}
+
 import IntCode
 
 import Control.Concurrent
-import Control.Exception (Exception,bracket,handle,throwIO)
-import Control.Monad (forM_,forever,void)
+import Control.Exception  (Exception,bracket,handle,throwIO)
+import Control.Monad      (forM_,forever,void)
 import Data.Array.IO
-import Data.Either (lefts,rights)
+import Data.Either        (lefts,rights)
 import Data.IORef
-import Data.List (unfoldr)
-import Data.List.Split (chunksOf)
-import Data.Vector ((//))
+import Data.List          (unfoldr)
+import Data.List.Split    (chunksOf)
+import Data.Vector        ((//))
 import System.Environment (getArgs)
-import System.Exit (exitFailure)
-import System.IO (hPutStrLn,stderr)
+import System.Exit        (exitFailure)
+import System.IO          (hPutStrLn,stderr)
 import Graphics.Vty hiding (Output)
 
 type Pos = (Int,Int)
 data Tile = Empty | Wall | Block | Paddle | Ball deriving (Eq,Enum)
 type TileOutput = (Pos,Tile)
-type ScoreOutput = Int
+newtype ScoreOutput = ScoreOutput { getScore :: Int }
 type Output = Either TileOutput ScoreOutput
-type JoystickTilt = Int
+data JoystickTilt = TiltLeft | TiltNeutral | TiltRight deriving Enum
+
+tiltFromInt :: Int -> JoystickTilt
+tiltFromInt = flip (.) (`compare` 0) $ \case LT -> TiltLeft
+                                             EQ -> TiltNeutral
+                                             GT -> TiltRight
+
+tiltToInt :: JoystickTilt -> Int
+tiltToInt = subtract 1 . fromEnum
 
 parseOutputs :: [Int] -> [Output]
 parseOutputs = map readTile . chunksOf 3
-  where readTile [-1,0,s] = Right s
+  where readTile [-1,0,s] = Right (ScoreOutput s)
         readTile [ x,y,t] = Left ((y,x),toEnum t)
-        readTile wtf = error $ "Error: got this \"tile\": " ++ show wtf
+        readTile wtf = error $ "Error: got this “tile”: " ++ show wtf
 
 -- The follow strategy: remember where the paddle is; send a direction
 -- event every time we see the ball.  This happens to work except for
@@ -37,24 +48,23 @@ follow s = unfoldr f (undefined,s) where
   f (_ ,Left ((_,px),Paddle):stream) =                f (px,stream)
   f (px,     _              :stream) =                f (px,stream)
   f (_ ,                   []      ) = Nothing
-  move px bx = signum (bx - px)
+  move px bx = tiltFromInt (bx - px)
 
 main :: IO ()
 main = do
   args <- getArgs
-  (get,ui,strategy,chan) <-
-    case args of
-      [] -> pure (getIntCode,False,follow,Nothing)
-      ["--visual",fileName] ->
-        pure (getIntCodeFromFile fileName,True,follow,Nothing)
-      ["--interactive",fileName] -> do
-        c <- newChan
-        inputs <- getChanContents c
-        pure (getIntCodeFromFile fileName,True,const inputs,Just c)
-      _ -> do
-        hPutStrLn stderr "Usage: {prg} < input"
-        hPutStrLn stderr "       {prg} {--visual|--interactive} input"
-        exitFailure
+  (get,ui,strategy,chan) <- case args of
+    [] -> pure (getIntCode,False,follow,Nothing)
+    ["--visual",fileName] ->
+      pure (getIntCodeFromFile fileName,True,follow,Nothing)
+    ["--interactive",fileName] -> do
+      c <- newChan
+      inputs <- getChanContents c
+      pure (getIntCodeFromFile fileName,True,const inputs,Just c)
+    _ -> do
+      hPutStrLn stderr "Usage: {prg} < input"
+      hPutStrLn stderr "       {prg} {--visual|--interactive} input"
+      exitFailure
 
   prg <- get
   let display0 = lefts $ parseOutputs $ evaluate prg []
@@ -67,12 +77,13 @@ main = do
 
   let (initialDisplay,updateStream) = splitAt (width * height) $
                                       parseOutputs $
-                                      evaluate hack controlStream
+                                      evaluate hack $
+                                      map tiltToInt controlStream
       controlStream = strategy (reverse initialDisplay ++ updateStream)
 
   case ui of
-    False -> do putStrLn blockReport
-                print $ last $ rights updateStream
+    False -> do print initialBlockCount
+                print $ getScore $ last $ rights updateStream
 
     True -> bracket (mkVty =<< standardIOConfig) shutdown $
             vtyUi blockReport width height initialDisplay updateStream chan
@@ -89,41 +100,51 @@ drawTile (y,x) Block  = char (attr `withForeColor` c) b
   where c = [red,yellow,green,cyan,blue,magenta] !! ((x + y) `mod` 6)
         b = "░▒▓" !! (y `mod` 3)
 
-data Quit = Quit deriving Show
-instance Exception Quit
+data Quit = Quit deriving (Show,Exception)
 
 vtyUi :: String -> Int -> Int
       -> [Output] -> [Output]
       -> Maybe (Chan JoystickTilt) -> Vty
       -> IO ()
 vtyUi blockReport width height initialDisplay updateStream chan vty = do
-  let infoImg = translate 0 3 $ string (attr `withStyle` italic) blockReport
+  let infoImg = string (attr `withStyle` italic) blockReport
+      instructionsImg = string attr "Use ← arrow keys → to move paddle" <->
+                        string attr "      down↓arrow stays in place" <->
+                        string attr "          q to quit"
+      instrImg = maybe (resize (imageWidth instructionsImg)
+                               (imageHeight instructionsImg) emptyImage)
+                       (const instructionsImg) chan
+      rightPane = instrImg <-> pad 0 2 0 2 infoImg
+
   raster <- newListArray ((0,0),(height-1,width-1))
                          (map snd (lefts initialDisplay))
-            :: IO (IOArray (Int,Int) Tile)
+            :: IO (IOArray Pos Tile)
   score <- newIORef 0
 
   handle (\Quit -> pure ()) $ do
 
+    -- If there's an open channel, translate and send keyboard events to it.
     forM_ chan $ \c -> do
       tid <- myThreadId
-      forkIO $ forever $ do
-        e <- nextEvent vty
-        case e of EvKey KLeft  [] -> writeChan c (-1)
-                  EvKey KDown  [] -> writeChan c   0
-                  EvKey KRight [] -> writeChan c   1
-                  EvKey (KChar 'q') [] -> throwTo tid Quit
-                  _ -> pure ()
-    
+      forkIO $ forever $ nextEvent vty >>=
+        \case EvKey KLeft  [] -> writeChan c TiltLeft
+              EvKey KDown  [] -> writeChan c TiltNeutral
+              EvKey KRight [] -> writeChan c TiltRight
+              EvKey (KChar 'q') [] -> throwTo tid Quit
+              _ -> pure ()
+
+    -- Main thread: update display as the VM demands it
     forM_ updateStream $ \d -> do
-      case d of Right s -> writeIORef score s
-                Left t  -> uncurry (writeArray raster) t
+      case d of Right (ScoreOutput s) -> writeIORef score s
+                Left (pos,t) -> writeArray raster pos t
       boardImg <- vertCat . map horizCat . chunksOf width .
                   map (uncurry drawTile) <$> getAssocs raster
       scoreImg <- string (attr `withStyle` bold) .
                   ("Score: " ++ ) . show <$> readIORef score
       update vty $ picForImage $
-        boardImg <|> pad 2 12 2 0 (scoreImg <-> infoImg)
-      sequence . (throwIO Quit <$) =<< nextEventNonblocking vty
+        boardImg <|> pad 2 3 2 0 (rightPane <-> scoreImg)
+      nextEventNonblocking vty >>= \case Just EvKey {} -> throwIO Quit
+                                         _             -> pure ()
       
+    -- After a complete run, give the user a chance to read the final score!
     void $ nextEvent vty
