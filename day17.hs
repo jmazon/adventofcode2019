cebd5c24a04c6d4aa8d422320ad5adc32cfb9ed2
Jean-Baptiste Mazon <jb@yavin>
2019-12-28 00:21:54 +0100

    Day 17 actual cleanup

--- day17.hs
+++ day17.hs
@@ -1,5 +1,5 @@
 -- Day 17: Set and Forget
-{-# LANGUAGE FlexibleContexts,FlexibleInstances #-}
+{-# LANGUAGE DeriveFoldable #-}
 
 import IntCode
 
@@ -8,7 +8,17 @@ import           Data.Char     (chr,ord)
 import           Data.List     (group,sort)
 import           Data.Maybe    (catMaybes)
 import qualified Data.Vector
-import           Control.Monad (guard)
+
+data Order = Forward | TurnLeft | TurnRight deriving (Eq,Show)
+type Pos = V2 Int
+data V2 n = V { x :: !n, y :: !n } deriving (Eq,Ord,Ix,Foldable)
+instance Num n => Num (V2 n) where
+  (V a b) + (V c d) = V (a+c) (b+d)
+  (V a b) * (V c d) = V (a*c - b*d) (a*d + b*c)
+  abs = undefined
+  signum = undefined
+  fromInteger = undefined
+  negate (V a b) = V (-a) (-b)
 
 main :: IO ()
 main = do
@@ -16,54 +26,46 @@ main = do
   let view = filter (not . null) $ lines $ map chr $ evaluate prg []
       width = length (head view)
       height = length view
-      grid = listArray ((0,0),(height-1,width-1)) $ concat view
+
+  let grid = listArray (V 0 0,V (height-1) (width-1)) $ concat view
       [startPos] = filter ((`elem` "^v<>") . (grid !)) (indices grid)
-      startDir | (grid!startPos) == '^' = (-1,0)
-      intersections = findIntersections grid
-      (_,path) = unzip $ follow grid startPos startDir
-      intersections2 = findIntersections2 path
-      outputs = evaluate (prg Data.Vector.// [(0,2)]) $ map ord solution
-  guard $ intersections == intersections2
-  print $ sum $ map (uncurry (*)) intersections
+      startDir | (grid!startPos) == '^' = V (-1) 0
+      (_,path) = follow grid startPos startDir
+      intersections = findIntersections path
+  print $ sum $ map product intersections
+
+  let outputs = evaluate (prg Data.Vector.// [(0,2)]) $ map ord solution
   print $ last outputs
 
-(!?) :: Array (Int,Int) a -> (Int,Int) -> Maybe a
-a !? p | inRange (bounds a) p = Just (a!p)
-       | otherwise = Nothing
+follow :: Array Pos Char -> Pos -> Pos -> ([Order],[Pos])
+follow grid pos0 dir0 = (orders,catMaybes path) where
+  (orders,path) = unzip $ walk pos0 dir0
+  walk pos dir
+    | walkable pos'         = (Forward, Just pos) : walk pos' dir
+    | walkable (pos + dirL) = (TurnLeft, Nothing) : walk pos  dirL
+    | walkable (pos + dirR) = (TurnRight,Nothing) : walk pos  dirR
+    | otherwise = []
+    where pos' = pos + dir
+          dirL = dir * i
+          dirR = -dirL
+          i = V 0 1
+          walkable p = inRange (bounds grid) p && grid!p == '#'
 
-isIntersection :: Array (Int,Int) Char -> (Int,Int) -> Bool
-isIntersection grid (i,j) = all ((== Just '#') . (grid !?))
-                            [(i,j),(i-1,j),(i+1,j),(i,j-1),(i,j+1)]
-
-findIntersections :: Array (Int,Int) Char -> [(Int,Int)]
-findIntersections grid = filter (isIntersection grid) (indices grid)
-
-data Order = Forward | TurnLeft | TurnRight deriving (Eq,Show)
-
-follow :: Array (Int,Int) Char -> (Int,Int) -> (Int,Int)
-       -> [(Order, Maybe (Int,Int))]
-follow grid pos dir
-  | grid !? pos' == Just '#' = (Forward,Just pos) : follow grid pos' dir
-  | grid !? (pos + dir * i) == Just '#' = (TurnLeft,Nothing) : follow grid pos (dir * i)
-  | grid !? (pos + dir * (-i)) == Just '#' = (TurnRight,Nothing) : follow grid pos (dir * (-i))
-  | otherwise = []
-  where pos' = pos + dir
-        i = (0,1)
-  
-instance Num (Int,Int) where
-  (a,b) + (c,d) = (a+c,b+d)
-  (a,b) * (c,d) = (a*c - b*d,a*d + b*c)
-  abs = undefined
-  signum = undefined
-  fromInteger = undefined
-  negate (a,b) = (-a,-b)
-
-findIntersections2 ::[Maybe (Int,Int)] -> [(Int,Int)]
-findIntersections2 = map head. filter (not . null . tail) . group . sort . catMaybes
+-- My initial intersection detection algorithm simply checked for
+-- plus-shaped patterns on the grid.  This works on AoC inputs, but
+-- can theoretically be foiled by having e.g. three parallel unspaced
+-- wires.
+--
+-- I wrote a wire-tracing algorithm for part2, and first tested it by
+-- having it report intersections too.  I'm keeping the latter
+-- intersection detection implementation, as the tracing has to be
+-- done anyway, and gives (unobservably) better results.
+findIntersections :: [Pos] -> [Pos]
+findIntersections = map head. filter (not . null . tail) . group . sort
 
 -- As most, I solved part 2 using my editor's search highlighting
 -- feature.  Here's my solution, as a testament to my second star of
--- the day!
+-- that day!
 solution :: String
 solution = unlines [ "B,A,B,A,C,A,C,B,C,C"
                    , "L,6,L,12,R,12,L,4"
