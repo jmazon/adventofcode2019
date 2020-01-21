155a47d674318d13c9867ea4de0e101429dd01bc
Jean-Baptiste Mazon <jb@yavin>
2020-01-21 01:14:29 +0100

    Use package modular-arithmetic for day 22

--- day22.hs
+++ day22.hs
@@ -1,34 +1,33 @@
 -- Day 22: Slam Shuffle
+{-# LANGUAGE DataKinds,TypeOperators #-}
 
-import Data.Int (Int32)
+import GHC.TypeLits (KnownNat)
+import Data.Semigroup
+import Data.Modular
+
+data Affine n = Affine !n !n
+instance Num n => Semigroup (Affine n) where
+  (Affine a1 b1) <> (Affine a2 b2) = Affine (a1*a2) (a2*b1+b2)
+instance Num n => Monoid (Affine n) where mempty = Affine 1 0
+
+apply :: Num n => Affine n -> n -> n
+apply (Affine a b) n = a*n + b
+
+inverse :: (Integral i,KnownNat n) => Affine (Mod i n) -> Affine (Mod i n)
+inverse (Affine a b) = Affine (inv a) (-b * inv a)
 
 main :: IO ()
 main = do
   shuffle <- map words . lines <$> getContents
 
-  let m = 10007 :: Int32
-      (a,b) = foldr (technique m) (1,0) shuffle
-  print $ (2019 - b) * inv m a `mod` m
+  let shuffle1 = foldMap technique shuffle
+  print (apply shuffle1 2019 :: ℤ/10007)
 
-  let m' = 119315717514047 :: Integer -- Int128 should be enough
-      (a',b') = pow (mult m') (foldr (technique m') (1,0) shuffle)
-                    101741582076661
-  print $ (2020*a' + b') `mod` m'
+  let shuffle2 = inverse (foldMap technique shuffle)
+  print (apply (stimes 101741582076661 shuffle2) 2020 :: ℤ/119315717514047)
 
-technique :: (Read i,Integral i) => i -> [String] -> (i,i) -> (i,i)
-technique m [_,"into",_, _] (a,b) = ((-a) `mod` m,(-1 - b) `mod` m)
-technique m ["cut",     _d] (a,b) = (a `mod` m,(b+read _d) `mod` m)
-technique m [_,"with",_,_d] (a,b) = (a*inv m (read _d) `mod` m,b*inv m (read _d) `mod` m)
-technique _        ws         _   = error $ "Unknown technique: " ++ unwords ws
-
-pow :: (t -> t -> t) -> t -> Int -> t
-pow op = go where go z 1             = z
-                  go z n | odd n     = z  `op` go z (n-1)
-                         | otherwise = p' `op` p'
-                    where p' = pow op z (n `div` 2)
-
-mult :: Integral i => i -> (i,i) -> (i,i) -> (i,i)
-mult m (a,b) (c,d) = (c*a `mod` m,(b*c+d) `mod` m)
-
-inv :: Integral i => i -> i -> i
-inv m z = pow (((`mod` m) .) . (*)) z (fromIntegral m-2)
+technique :: (Read i,Integral i,KnownNat n) => [String] -> Affine (Mod i n)
+technique [_,"into",_, _] = Affine (-1) (-1)
+technique ["cut",     _d] = Affine 1 (-read _d)
+technique [_,"with",_,_d] = Affine (read _d) 0
+technique        ws       = error $ "Unknown technique: " ++ unwords ws
